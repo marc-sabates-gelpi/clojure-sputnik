78278f7f0ff3c81c1bb5b6ea7b6c79415c7d6750
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2017-06-20 22:18:57 +0100

    Listen socket & evals input

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "GNU General Public License (GPL) version 3"
             :url "https://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [server-socket "1.0.0"]]
   :main ^:skip-aot sputnik.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/sputnik/core.clj
+++ src/sputnik/core.clj
@@ -1,10 +1,26 @@
 (ns sputnik.core
+  (:use [clojure.java.io :only [reader writer]]
+        [server.socket :only [create-server]])
   (:gen-class))
 
 ;; Objectives:
 ;; * Function to emit the periodic signal
 ;; * Listen to on TCP port for new periodic signal functions
 ;; * Persist periodic signal function
+
+(defn- signals-server [in out]
+  (binding [*in* (reader in)
+            *out* (writer out)
+            *err* (writer System/err)]
+
+    (print "\nType EDN: ") (flush)
+    (loop [input (read-line)]
+      (when input
+        (println (eval (read-string input)))
+        (.flush *err*)
+        (print "\nType EDN: ") (flush)
+        (recur (read-line))))
+    ))
 
 (defn set-interval [callback ms] 
   (future
@@ -15,12 +31,13 @@
 
 (defn -main
   [& args]
+  (defonce server (create-server (Integer. 3333) signals-server))
   (def bip (set-interval #(println "Bip!") 5000))
-  (println "First action after setting the periodic signal"))
+  (println "First action after setting the periodic signal")
   (future (do
             (Thread/sleep 30000)
             (println "Going to cancel previous periodic signal")
             (future-cancel bip)
             (println "Going to kick a new periodic signal")
-            (set-interval #(println "Bop!") 5000)))
+            (set-interval #(println "Bop!") 5000))))
 
